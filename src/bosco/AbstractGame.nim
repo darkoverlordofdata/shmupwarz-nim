f33ae5fcd40ad91cc9278856fb7621c75e012fa2
bruce <darkoverlordofdata@gmail.com>
2016-02-19 16:55:23 -0800

    daily

--- src/bosco/AbstractGame.nim
+++ src/bosco/AbstractGame.nim
@@ -17,55 +17,16 @@ type
     ticks* : uint32
     lastTick : uint32
 
+proc init_sdl(this : AbstractGame)
 proc start*(this : AbstractGame) :  int {.discardable.}
+proc render*(this : AbstractGame)
 method event*(this : AbstractGame, evt : Event) {.base.}
 method update*(this : AbstractGame, delta : float64) {.base.}
-proc render*(this : AbstractGame)
-proc init*(this : AbstractGame) : bool
 method cleanup*(this : AbstractGame) {.base.}
+method initialize*(this : AbstractGame)
 
-proc start*(this : AbstractGame) : int =
-
-  this.running = this.init()
-  if not this.running : return -1
-
-  this.currentKeyStates = getKeyboardState(nil)
-  var evt = defaultEvent
-  while this.running:
-    while pollEvent(evt):
-      case evt.kind
-        of QuitEvent:
-          this.running = false
-        #else: discard
-        else: this.event(evt)
-    if (bool)this.currentKeyStates[41] : this.running = false
-    this.ticks = getTicks()
-    this.delta = float64(this.ticks - this.lastTick)/1000.0
-    this.lastTick = this.ticks
-    this.update(this.delta)
-    this.render()
-    destroy this.renderer
-    destroy this.window
-
-  this.cleanup()
-  return 0
-
-method event*(this : AbstractGame, evt : Event)  =
-  return
-
-method update*(this : AbstractGame, delta : float64) =
-  return
-
-proc render*(this : AbstractGame) =
-
-  this.renderer.setDrawColor(0, 0, 0, 255)
-  this.renderer.clear()
-  for sprite in this.sprites:
-    sprite.render(this.renderer)
-  this.renderer.present()
-
-proc init*(this : AbstractGame) : bool =
-
+proc init_sdl(this : AbstractGame) =
+  ## Initialize SDL layer
   discard sdl2.init(INIT_EVERYTHING)
   discard image.init(IMG_INIT_PNG)
 
@@ -79,7 +40,50 @@ proc init*(this : AbstractGame) : bool =
       echo "Error creating SDL_Renderer: ", getError()
       quit 1
 
-  return true
+proc start*(this : AbstractGame) : int =
+  ## Start the game
+  this.init_sdl()
+  this.initialize()
+  this.currentKeyStates = getKeyboardState(nil)
+  var evt = defaultEvent
+  while this.running:
+    while pollEvent(evt):
+      case evt.kind
+        of QuitEvent:
+          this.running = false
+        else: this.event(evt)
+    if (bool)this.currentKeyStates[41] : this.running = false
+    this.ticks = getTicks()
+    this.delta = float64(this.ticks - this.lastTick)/1000.0
+    this.lastTick = this.ticks
+    this.update(this.delta)
+    this.render()
+
+  destroy this.renderer
+  destroy this.window
+  this.cleanup()
+  return 0
+
+proc render*(this : AbstractGame) =
+  ## Render the frame
+  this.renderer.setDrawColor(0, 0, 0, 255)
+  this.renderer.clear()
+  for sprite in this.sprites:
+    sprite.render(this.renderer)
+  this.renderer.present()
+
+method initialize*(this : AbstractGame) =
+  ## Abstract method: initialize the game
+  return
+
+method event*(this : AbstractGame, evt : Event)  =
+  ## Abstract method: receive SDL events
+  return
+
+method update*(this : AbstractGame, delta : float64) =
+  ## Abstract update: update the game system
+  return
 
 method cleanup*(this : AbstractGame) =
+  ## Abstract cleanup: teardown the game
   return
--- src/old.nim
+++ src/old.nim
@@ -6,13 +6,26 @@ import sdl2/ttf
 import bosco/ECS
 import bosco/Sprite
 import bosco/AbstractGame
-import ComponentEx
-import MatchEx
-import WorldEx
-import EntityEx
+import Components
+import Match
 
 type
-
+  # Components* = enum
+  #   Position
+  #   Movement
+  #   Resource
+  #
+  # PositionComponent* = ref object of IComponent
+  #   x* : float64
+  #   y* : float64
+  #
+  # MovementComponent* = ref object of IComponent
+  #   x* : float64
+  #   y* : float64
+  #
+  # ResourceComponent* = ref object of IComponent
+  #   path* : string
+  #
   TestSystem* = ref object of System
     game* : Game
     group* : Group
@@ -21,19 +34,36 @@ type
     world* : World
     player* : Entity
 
+proc addPosition(this : Entity, x : float64, y : float64) : Entity =
+  let pos = PositionComponent()
+  pos.x = x
+  pos.y = y
+  return this.addComponent(int(Position), pos)
+
+proc addMovement(this : Entity, x : float64, y : float64) : Entity =
+  let mov = MovementComponent()
+  mov.x = x
+  mov.y = y
+  return this.addComponent(int(Movement), mov)
+
+proc addResource(this : Entity, path : string) : Entity =
+  let res = ResourceComponent()
+  res.path = path
+  return this.addComponent(int(Resource), res)
+
 proc newTestSystem*(game : Game) : TestSystem =
   new(result)
   result.game = game
 
 method initialize*(this : TestSystem) =
-  this.group = this.world.getGroup(Match.Resource)
+  this.group = this.world.getGroup(MatchAllOf(@[int(Resource)]))
   let e1 = this.group.getSingleEntity()
   echo "Entity = ", $e1
 
 proc createPlayer(this : Game) : Entity =
   return this.world.createEntity("player")
   .addPosition(0, 0)
-  .addVelocity(0, 0)
+  .addMovement(0, 0)
   .addResource("res/images/fighter.png")
 
 proc constructor*(this : Game, name : string, width : cint, height : cint) =
@@ -44,7 +74,7 @@ proc constructor*(this : Game, name : string, width : cint, height : cint) =
 
 method initialize*(this : Game) =
   var cp : seq[string] = @[]
-  for x in Component: cp.add $x
+  for x in Components: cp.add $x
   this.world = newWorld(cp)
   this.player = this.createPlayer()
   this.world.add(newTestSystem(this))
